59d04dc46b4290cf38ff1dd4368f7f55980ac03a
Sounava Mandal <157009150+CeleritasCodingZen@users.noreply.github.com>
2024-02-12 17:19:50 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': '350c83824dmsh882a13f66a78478p1a4a89jsn867851458201',
+    'X-RapidAPI-Key': 'your_key',
   },
 };
 
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': '350c83824dmsh882a13f66a78478p1a4a89jsn867851458201',
+    'X-RapidAPI-Key': 'your_key',
   },
 };
 
